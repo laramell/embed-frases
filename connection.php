2e6115d701462971f0ae77c0a74b4f7626ed1076
Lara Mel <96661440+laramell@users.noreply.github.com>
2024-05-25 20:38:18 -0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,68 @@
+<?php
+// Definindo constantes para a URL do Supabase e a API Key
+define('SUPABASE_URL', 'https://wcmxslbooabrkgqqrhax.supabase.co');
+define('SUPABASE_API_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjbXhzbGJvb2FicmtncXFyaGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2NzY0NDcsImV4cCI6MjAzMjI1MjQ0N30.SDYqoomslpLU5m3qEoW85P8nVbRpF07T0Zjpl2Gbg18');
+
+// Função para realizar requisições cURL
+function executeCurl($url, $method = 'GET', $data = null) {
+    $headers = [
+        "apikey: " . SUPABASE_API_KEY,
+        "Authorization: Bearer " . SUPABASE_API_KEY,
+        "Content-Type: application/json"
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    if ($method == 'POST' && $data) {
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    } elseif ($method == 'GET') {
+        curl_setopt($ch, CURLOPT_HTTPGET, true);
+    }
+
+    $response = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $error = curl_error($ch);
+
+    curl_close($ch);
+
+    if ($httpcode >= 400) {
+        echo "Erro na requisição: $httpcode<br>";
+        echo "Resposta: $response<br>";
+        return false;
+    } elseif ($error) {
+        echo "Erro cURL: $error<br>";
+        return false;
+    }
+
+    return json_decode($response, true);
+}
+
+// Função para obter o número total de frases
+function getTotalQuotes() {
+    $url = SUPABASE_URL . "/rest/v1/frases?select=count";
+    return executeCurl($url);
+}
+
+// Função para obter uma frase aleatória
+function getRandomQuote() {
+    $totalQuotes = getTotalQuotes();
+    if ($totalQuotes && isset($totalQuotes[0]['count'])) {
+        $randomOffset = rand(0, $totalQuotes[0]['count'] - 1);
+        $url = SUPABASE_URL . "/rest/v1/frases?select=*&limit=1&offset=$randomOffset";
+        return executeCurl($url);
+    }
+    return false;
+}
+
+// Função para inserir uma nova frase
+function insertQuote($citacao_frase, $autor_frase) {
+    $url = SUPABASE_URL . "/rest/v1/frases";
+    $data = [
+        'citacao_frase' => $citacao_frase,
+        'autor_frase' => $autor_frase
+    ];
+    return executeCurl($url, 'POST', $data);
+}
