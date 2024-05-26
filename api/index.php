2150457e5a3727a5a0c5c6a8f7123c5f64713f2c
Lara Mel <96661440+laramell@users.noreply.github.com>
2024-05-25 23:27:19 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -17,6 +17,9 @@ $quote = getRandomQuote();
 
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+            * {
+                overflow: hidden;
+            }
             #content {
                 display: flex;
                 justify-content: center;
